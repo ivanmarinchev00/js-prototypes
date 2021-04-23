50f613c450ebf18f4f9620f048181bb47e0a55ea
ivanmarinchev00 <ivanmarinchev00@gmail.com>
2021-04-23 18:07:25 +0200

    improved dropdown

--- script.js
+++ script.js
@@ -1,59 +1,52 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     var myCustomDropDown = {
-//         emptyText: 'Select some option',
-//         items: [{
-//            text: 'option1',
-//            value: 1
-//         }, {
-//            text: 'option1',
-//            value: 1
-//         }]
-//     };
-
-//     var _select = document.createElement("select");
-//     myCustomDropDown.items.map(item => {
-//         _select.options[_select.options.length] = new Option(item.text, item.id);
-//     })
-
-//     document.body.appendChild(_select);
-// })
-
-myCustomDropDownMenu = function (options) {
-  this.options = options;
-};
-
-myCustomDropDownMenu.prototype = {
-  init: function () {
-    this.render();
-  },
-  render: function (div, items) {
-    const currentDiv = document.getElementById(div);
-    var _select = document.createElement("select");
-    items.array.forEach((element) => {
-        options += document.createTextNode(`
-            <option value="${element.value}">${element.text}</option>
-            `);
+document.addEventListener("DOMContentLoaded", () => {
+  myCustomDropDownMenu = function (options) {
+    this.options = options;
+    this.init(options);
+  };
+  
+  myCustomDropDownMenu.prototype = {
+    init: function (options) {
+      console.log(options.items + "OPTIONS ITEMS");
+      this.render(options.renderTo, options.items)
+      
+    },
+    render: function (div, items) {
+      
+      var currentDiv = document.getElementById(`${div}`);
+      console.log(items + "ITEMS HERE")
+      console.log(currentDiv + "SHOULD BE DIV")
+      var _select = document.createElement("select");
+      items.forEach((element) => {
+        _select.innerHTML += `
+              <option value="${element.value}">${element.text}</option>
+              `;
+      });
+      // _select.append(items);
+      currentDiv.appendChild(_select);
+      document.body.appendChild(currentDiv);
+    },
+  };
+  
+  
+    const dropDown = new myCustomDropDownMenu({
+      renderTo: "myDropDownContainer",
+      emptyText: "Select some option",
+      items: [
+        {
+          text: "option1",
+          value: 1,
+        },
+        {
+          text: "option1",
+          value: 1,
+        },
+      ],
     });
-    _select.appendChild(options);
-    currentDiv.appendChild(_select);
-    document.append(currentDiv);
-  },
-};
+  
+  
+    dropDown.init(dropDown.options.renderTo, dropDown.options.items)
+})
 
-  const dropDown = new myCustomDropDownMenu({
-    render: "#myDropDownContainer",
-    emptyText: "Select some option",
-    items: [
-      {
-        text: "option1",
-        value: 1,
-      },
-      {
-        text: "option1",
-        value: 1,
-      },
-    ],
-  });
 
-  console.log(dropDown.options);
+
 
